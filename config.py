dd5af2a5688b6519e3df463cb62b9de832b38d22
bharat choudhary <bharatchoudhary817@gmail.com>
2020-10-17 00:19:16 +0530

    Third

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import transformers
 
-DEVICE = "cuda"
-MAX_LEN = 300
+DEVICE = "cpu"
+MAX_LEN = 32
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
 ACCUMULATION = 2
-BERT_PATH = "bert_base_unchased/"
-MODEL_PATH = "model.bin"
-TRAINING_FILE = "IMDB Dataset.csv"
+BERT_PATH = "input/bert_base_unchased/"
+MODEL_PATH = "pytorch_model.bin"
+TRAINING_FILE = "input/IMDB Dataset.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH,
     do_low_case=True
